748e66232b202254e01a969b1d0f607851468bb6
EZ118 <eason1018@outlook.com>
2021-08-08 18:00:25 +0800

    header of ctrump
    import this header in your code.

---
+++ ctrump.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+#define $ ;
+#define donate cout
+#define earn cin
+#define trump_gives_back return
+#define money long long
+#define trump_law namespace
+#define instead std
+#define rule void
+#define news bool
+#define fake false
+#define non_fiction true
+#define pocket_money int 
+
